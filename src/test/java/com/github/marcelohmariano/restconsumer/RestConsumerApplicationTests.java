ae63ba7eced376fbb93855394c8eb87112147084
Marcelo Mariano <marcelo.hs.mariano@gmail.com>
2021-01-28 23:23:29 -0300

    Reformat files

--- src/test/java/com/github/marcelohmariano/restconsumer/RestConsumerApplicationTests.java
+++ src/test/java/com/github/marcelohmariano/restconsumer/RestConsumerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RestConsumerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
